a613cc7afdbf8eb264819028a5efd7e1ab3279d2
AlinaLosk <161211124+AlinaLosk@users.noreply.github.com>
2024-04-23 20:22:59 +0300

    Add files via upload

---
+++ for_cat.py
@@ -0,0 +1,11 @@
+from pet_frends import Cat
+
+cat1 = Cat('Baron', 'мальчик', '2' )
+cat2 = Cat('Sem', 'маьчик', '2')
+
+print('cat1.name=', cat1.name)
+print('cat1.gender=', cat1.gender)
+print('cat1.age=', cat1.age)
+print('cat2.name=', cat2.name)
+print('cat2.gender=', cat2.gender)
+print('cat2.age=', cat2.age)
